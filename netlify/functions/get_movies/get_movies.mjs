09db2f78bad1f422adb65bae18436a242f2a1fd8
Laura Morgan <lauramorgan833@gmail.com>
2025-01-18 12:21:20 -0600

    netlify function logs

--- netlify/functions/get_movies/get_movies.mjs
+++ netlify/functions/get_movies/get_movies.mjs
@@ -3,11 +3,12 @@ const { MongoClient } = require("mongodb");
 const handler = async (event) => {
     console.log("get_movies function called");
     try {
-        const mongoClient = new MongoClient(process.env.MONGODB_URI);
-        console.log("Connecting to MongoDB");
         console.log("Database: ", process.env.MONGODB_DATABASE);
         console.log("Collection: ", process.env.MONGODB_COLLECTION);
         console.log("URI: ", process.env.MONGODB_URI);
+        const mongoClient = new MongoClient(process.env.MONGODB_URI);
+        console.log("Connecting to MongoDB");
+        
         const clientPromise = mongoClient.connect();
         const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
         const collection = database.collection(process.env.MONGODB_COLLECTION);
